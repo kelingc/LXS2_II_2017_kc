ec4675eb3592ff498ac95e278a6fe3bd63769b03
Keling Caballero <kelingc_15@hotmail.com>
2017-03-18 16:20:48 -0600

    Version 2 del IF

--- src/parametros.sh
+++ src/parametros.sh
@@ -5,8 +5,13 @@ argumentos=3
 if [ $# -eq $argumentos ]
 then
      echo El numero de parametros fue correcto
+     if [ $1 = perro ]
+     then
+           echo El parametro '$1' fue $1
+     fi            
 else
      echo El numero de parametros fue incorrecto
+     echo Se pasaron $# argumentos
 fi
 
 echo fin de script
