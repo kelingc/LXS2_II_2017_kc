e225183e9d43f267d0575d0f11f22a4d8601a507
Keling Caballero <kelingc_15@hotmail.com>
2017-03-18 16:09:27 -0600

    Inicio del script de parametros

---
+++ src/parametros.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+argumentos=3
+
+if [ $# -eq $argumentos ]
+then
+     echo El numero de parametros fue correcto
+else
+     echo El numero de parametros fue incorrecto
+fi
+
+echo fin de script
+exit 0
